27eff73d9e0d759374631445d85883f1f814fa4e
Devin <devneal@nealscape.mit.edu>
2017-06-11 18:09:50 -0400

    made about page responsive

---
+++ script.js
@@ -0,0 +1,10 @@
+function setWidth(id, width) {
+    var smallscreen = window.matchMedia("(max-width: 768px)");
+
+    if (!smallscreen) {
+        tag = document.getElementById(id);
+        tag.style.width = width;
+    }
+
+    return 0;
+}
