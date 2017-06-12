d85ebb361ab492f524463120f99e6406b4e7a72b
Devin <devneal@nealscape.mit.edu>
2017-06-12 09:44:45 -0400

    fixed buttons again

--- script.js
+++ script.js
@@ -8,3 +8,18 @@ function setWidth(id, width) {
 
     return 0;
 }
+
+function adjustbuttons() {
+    nav_buttons = document.getElementsByClassName("nav-button");
+    var smallscreen = window.matchMedia("(max-width: 768px)");
+
+    if (smallscreen.matches) {
+        for (let i=0; i<nav_buttons.length; i++) {
+            nav_buttons[i].style.width = "100%";
+        }
+    } else {
+        for (let i=0; i<nav_buttons.length; i++) {
+            nav_buttons[i].style.width = "95%";
+        }
+    }
+}
